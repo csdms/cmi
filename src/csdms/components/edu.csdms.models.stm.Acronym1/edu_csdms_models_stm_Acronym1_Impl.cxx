f906c7e28df6de4d255cd1e7bb18d6484019a6a0
huttone <huttone@aa4c52fb-b5f8-4c7d-84f0-06fd22a65a58>
2010-12-17 18:20:29 +0000

    * Fixed use of wrong component name

--- src/csdms/components/edu.csdms.models.stm.Acronym1/edu_csdms_models_stm_Acronym1_Impl.cxx
+++ src/csdms/components/edu.csdms.models.stm.Acronym1/edu_csdms_models_stm_Acronym1_Impl.cxx
@@ -453,7 +453,7 @@ edu::csdms::models::stm::Acronym1_impl::initialize_impl (
 
       tmpls.add_file ("STM_Acronym1.txt.in", in_file);
 
-      tmpls.substitute (userinput, "/STM/1DDeltaBW/Input/Var/", work_dir);
+      tmpls.substitute (userinput, "/STM/Acronym1/Input/Var/", work_dir);
 
     } 
     else
