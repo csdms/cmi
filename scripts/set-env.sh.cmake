c04852b835c4d3f8f9fa215e41a762a06c97b91d
huttone <huttone@aa4c52fb-b5f8-4c7d-84f0-06fd22a65a58>
2010-12-20 21:55:24 +0000

    * Add configure scripts to set environment
      Create configure scripts that set up a user's environment to use this
      project.  cmake will generate two new scripts to do this,
        set-env.sh: Source this file to set environment variables
        @PROJECT_NAME@: A module file to set the environment
    
      cmake also now generates a bocca-gui-backend.sh file that correctly
      sets the user's environment.

---
+++ scripts/set-env.sh.cmake
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Source this file to load the modules required for the CSDMS project and
+## to set required environment variables
+
+source /usr/local/modules/init/bash
+module load openmi cem/devel sedflux/devel hydrotrend/3.0 child csdms roms stm
+
+if [ -z "${CMT_PROJECT_DIALOG_PATH+x}" ] ; then
+  export CMT_PROJECT_DIALOG_PATH=@CONFIG_DIR@
+fi
+if [ -z "${CMT_PROJECT_TEMPLATE_PATH+x}" ] ; then
+  export CMT_PROJECT_TEMPLATE_PATH=@TEMPLATE_DIR@
+fi
+
