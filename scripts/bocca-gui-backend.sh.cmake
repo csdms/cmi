101863af4e5f91256fe4838bcf21b5e8f9543f38
huttone <huttone@aa4c52fb-b5f8-4c7d-84f0-06fd22a65a58>
2011-01-24 23:25:34 +0000

    * Add marssim to list of components to load

--- scripts/bocca-gui-backend.sh.cmake
+++ scripts/bocca-gui-backend.sh.cmake
@@ -1,6 +1,7 @@
 #!/bin/bash
 source /usr/local/modules/init/bash
 module load openmi cem/devel sedflux/devel hydrotrend/3.0 child csdms roms stm
+module load marssim
 
 if [ -z "${CMT_PROJECT_DIALOG_PATH+x}" ] ; then
   export CMT_PROJECT_DIALOG_PATH=@CONFIG_DIR@
